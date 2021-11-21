b48944ca6e22fe8a3f4278f0fa1ba4dea5de0b80
Alex Maxim <55588549+almaxim@users.noreply.github.com>
2021-11-21 10:50:23 -0500

    Add files via upload

---
+++ js/effects.js
@@ -0,0 +1,38 @@
+
+var w = document.getElementById('answerButton1');
+var x = document.getElementById('answerButton2');
+var y = document.getElementById('answerButton3');
+var z = document.getElementById('answerButton4');
+
+function ShowAndHide1() {
+    if (w.style.visibility == 'hidden') {
+        w.style.visibility = 'visible';
+    } else {
+        w.style.visibility = 'hidden';
+    }
+}
+
+function ShowAndHide2() {
+    if (x.style.visibility == 'hidden') {
+        x.style.visibility = 'visible';
+    } else {
+        x.style.visibility = 'hidden';
+    }
+}
+
+function ShowAndHide3() {
+    if (y.style.visibility == 'hidden') {
+        y.style.visibility = 'visible';
+    } else {
+        y.style.visibility = 'hidden';
+    }
+}
+
+function ShowAndHide4() {
+    if (z.style.visibility == 'hidden') {
+        z.style.visibility = 'visible';
+    } else {
+        z.style.visibility = 'hidden';
+    }
+}
+
